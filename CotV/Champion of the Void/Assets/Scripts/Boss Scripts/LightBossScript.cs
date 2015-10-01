dae28dc9630d8bb7e468ec7705ad7c5644877599
Austin Willoughby <arw8020@rit.edu>
2015-10-01 12:14:32 -0400

    Working on light boss implementation in different scene.  Attempting to make him move and turn towards the nearest player.

---
+++ CotV/Champion of the Void/Assets/Scripts/Boss Scripts/LightBossScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBossScript : MonoBehaviour
+{
+    private Light spot;
+    private GameObject[] players = new GameObject[2];
+    private GameObject fixatePlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+        spot = transform.FindChild("Spotlight").GetComponent<Light>();
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fixate();
+    }
+
+    private void Fixate()
+    {
+        float p1Dist = Vector3.Distance(transform.position, players[0].transform.position);
+        float p2Dist = Vector3.Distance(transform.position, players[1].transform.position);
+        if (p1Dist < p2Dist)
+        {
+            fixatePlayer = players[0];
+        }
+        else
+        {
+            fixatePlayer = players[1];
+        }
+    }
+
+    private void TurnToFace()
+    {
+        Vector3 targetVector = fixatePlayer.transform.position - transform.position;
+    }
+}
