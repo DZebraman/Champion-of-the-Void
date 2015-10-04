14d3e4dd6b84119d57c25e25c2962cec0f25451b
DigitalSeduction <rpark682@gmail.com>
2015-10-04 13:53:34 -0400

    Floor now reflects things.

---
+++ CotV/Champion of the Void/Assets/Scripts/Camera/ReflectionManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReflectionManager : MonoBehaviour {
+    ReflectionProbe probe;
+
+    // Use this for initialization
+    void Start()
+    {
+        probe = GetComponent<ReflectionProbe>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        probe.transform.position = new Vector3(Camera.main.transform.position.x,-Camera.main.transform.position.y,Camera.main.transform.position.z);
+
+        probe.RenderProbe();
+	}
+}
